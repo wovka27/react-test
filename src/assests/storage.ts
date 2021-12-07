2f5370039ddac7b2f36fab17c6e38715aff0f1a3
Пользователь <wovka.home@gmail.com>
2021-12-07 16:28:24 +0500

    delete unnecessary files

--- src/assests/storage.ts
+++
@@ -1,5 +0,0 @@
-export const storage = {
-  getItem: (name: string) => localStorage.getItem(name),
-  setItem: (name: string, value: string) =>
-    window.localStorage && localStorage.setItem(name, value),
-};
